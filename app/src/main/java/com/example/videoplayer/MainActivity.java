f918793df5ae699a97e73a4137c17481b8a64812
Cizor <yourcizor@gmail.com>
2019-03-27 09:38:45 +0530

    Follwing features added as first commit: 1. MP3 is stored in raw res package 2. Pause, Start, Rew and For buttons added. Text Views for Max durationa and running duration added. SeekBar depicting running duration added. 3. Click on Rew and For button does 5 secs of Rewind and Forward respectively.

---
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.videoplayer;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView maxTime;
+    private TextView currPosition;
+    private Button pauseButton;
+    private Button startButton;
+    private Button forwardButton;
+    private Button rewindButton;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+    private Handler threadHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.maxTime = findViewById(R.id.textView2);
+        this.currPosition = findViewById(R.id.textView3);
+        this.pauseButton = findViewById(R.id.button3);
+        this.startButton = findViewById(R.id.button2);
+        this.forwardButton = findViewById(R.id.button4);
+        this.rewindButton = findViewById(R.id.button);
+        this.pauseButton.setEnabled(false);
+        this.seekBar = findViewById(R.id.seekBar);
+        this.seekBar.setClickable(false);
+
+        int songId = this.getResources().getIdentifier("perfect", "raw", this.getPackageName());
+        this.mediaPlayer = MediaPlayer.create(this, songId);
+        this.forwardButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int currentPosition = mediaPlayer.getCurrentPosition();
+                int duration = mediaPlayer.getDuration();
+                int LONG_TIME = 10000;
+                if (currentPosition+LONG_TIME < duration){
+                    mediaPlayer.seekTo(currentPosition+LONG_TIME);
+                } else {
+                    mediaPlayer.seekTo(duration);
+                }
+                return true;
+            }
+        });
+
+        this.rewindButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int currentPosition = mediaPlayer.getCurrentPosition();
+                int LONG_TIME = 10000;
+                if (currentPosition - LONG_TIME > 0){
+                    mediaPlayer.seekTo(currentPosition - LONG_TIME);
+                } else {
+                    mediaPlayer.seekTo(0);
+                }
+                return true;
+            }
+        });
+    }
+
+    private String msToString(int millisecs){
+        long minutes = TimeUnit.MILLISECONDS.toMinutes((long)millisecs);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds((long)millisecs);
+        return minutes+":"+seconds;
+    }
+
+    public void doStart(View v){
+        int duration = this.mediaPlayer.getDuration();
+        int currentPosition = this.mediaPlayer.getCurrentPosition();
+        if (currentPosition == 0){
+            this.seekBar.setMax(duration);
+            String maxTimeString = this.msToString(duration);
+            this.maxTime.setText(maxTimeString);
+        } else if(currentPosition == duration){
+            this.mediaPlayer.reset();
+        }
+        this.mediaPlayer.start();
+        UpdateSeekBarThread updateSeekBar = new UpdateSeekBarThread();
+        threadHandler.postDelayed(updateSeekBar, 50);
+        this.pauseButton.setEnabled(true);
+        this.startButton.setEnabled(true);
+    }
+    class UpdateSeekBarThread implements Runnable{
+        @Override
+        public void run() {
+            int currentPosition = mediaPlayer.getCurrentPosition();
+            String currPosStr = msToString(currentPosition);
+            currPosition.setText(currPosStr);
+
+            seekBar.setProgress(currentPosition);
+            threadHandler.postDelayed(this, 50);
+        }
+    }
+
+    public void doPause(View v){
+        this.mediaPlayer.pause();
+        this.pauseButton.setEnabled(false);
+        this.startButton.setEnabled(true);
+    }
+
+    public void doRewind(View v){
+        int currentPosition = mediaPlayer.getCurrentPosition();
+        int SUBTRACT_TIME = 5000;
+        if (currentPosition - SUBTRACT_TIME > 0){
+            this.mediaPlayer.seekTo(currentPosition - SUBTRACT_TIME);
+        }else {
+            mediaPlayer.seekTo(0);
+        }
+    }
+
+    public void doFastForward(View v){
+        int currentPosition = mediaPlayer.getCurrentPosition();
+        int duration = mediaPlayer.getDuration();
+        int ADD_TIME = 5000;
+
+        if (currentPosition + ADD_TIME < duration){
+            this.mediaPlayer.seekTo(currentPosition+ADD_TIME);
+        } else {
+            mediaPlayer.seekTo(duration);
+        }
+    }
+}
